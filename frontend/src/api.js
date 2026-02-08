3e1d88aa4cc6d9910c90f568995d5ac2493fef51
Akhiljithu <jithuakhil15@gmail.com>
2026-02-08 12:41:46 +0530

    use VITE_API_URL from vercel env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,10 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://personal-expense-tracker-3-hdz2.onrender.com/api';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://personal-expense-tracker-3-hdz2.onrender.com/api';
 
-export const USER_ID = 'primary_user';
+// export const USER_ID = 'primary_user';
+// export default API_BASE_URL;
+
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+export const USER_ID = "primary_user";
 export default API_BASE_URL;
